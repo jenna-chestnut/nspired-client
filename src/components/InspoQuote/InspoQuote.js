9b26eeeca4edd11ae4ee288f302d7212307da8d4
Jenna Chestnut <jennabot5000@gmail.com>
2020-12-30 09:25:17 -0600

    inspo quote generator

--- src/components/InspoQuote/InspoQuote.js
+++ src/components/InspoQuote/InspoQuote.js
@@ -1,17 +1,23 @@
 import './InspoQuote.css';
 import React from 'react';
 import NSpiredContext from '../../contexts/NSpiredContext';
+import InspoService from '../../services/inspo-api-service';
 
 class InspoQuote extends React.Component {
   static contextType = NSpiredContext;
 
   componentDidMount() {
-    
+    InspoService.getInspo()
+    .then(this.context.setInspo)
+    .catch(this.context.setError)
   }
 
   render() {
+    const { inspo = [] } = this.context;
+    const quote = inspo[Math.floor(Math.random() * inspo.length)] || '';
+
       return (
-        <p className="inspiration">Make your life a masterpiece.</p>
+        <p className="inspiration">{quote.inspo_quote}</p>
       );
     }
 }
--- src/services/inspo-api-service.js
+++ src/services/inspo-api-service.js
@@ -1,54 +1,17 @@
 import config from '../config'
-import TokenService from './token-service'
 
 const InspoService = {
-  // *** ADVICE ***
+  // *** INSPO ***
 
-  // GET advice for specific goal
-  getGoalAdvice(goalId) {
-    return fetch(`${config.API_ENDPOINT}/advice/${goalId}`, {
-      headers: {
-        'authorization': `bearer ${TokenService.getAuthToken()}`
-      },
-    })
+  // GET inspo
+  getInspo() {
+    return fetch(`${config.API_ENDPOINT}/inspo`)
       .then(res =>
         (!res.ok)
           ? res.json().then(e => Promise.reject(e))
           : res.json()
       )
-  },
-
-  // POST advice for specific goal
-  postAdvice(goal_id, advice_text) {
-    return fetch(`${config.API_ENDPOINT}/advice/${goal_id}`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'authorization': `bearer ${TokenService.getAuthToken()}`
-      },
-      body: JSON.stringify(advice_text),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  },
-
-    // DELETE user advice
-    deleteUserAdvice(goal_id) {
-      return fetch(`${config.API_ENDPOINT}/advice/${goal_id}`, {
-        method: 'DELETE',
-        headers: {
-          'authorization': `bearer ${TokenService.getAuthToken()}`
-        }
-      })
-        .then(res =>
-          (!res.ok)
-            ? res.json().then(e => Promise.reject(e))
-            : res
-        )
-    },
+  }
 }
 
 export default InspoService
